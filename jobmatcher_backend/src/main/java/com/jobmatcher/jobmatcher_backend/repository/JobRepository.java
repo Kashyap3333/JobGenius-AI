3276f609a45454b80258be9260b59fb00bac88b2
Kashyap Gajera <kashyapgajera30@gmail.com>
2026-04-25 00:01:10 +0530

    Job Matching completed

--- jobmatcher_backend/src/main/java/com/jobmatcher/jobmatcher_backend/repository/JobRepository.java
+++ jobmatcher_backend/src/main/java/com/jobmatcher/jobmatcher_backend/repository/JobRepository.java
@@ -3,5 +3,8 @@ package com.jobmatcher.jobmatcher_backend.repository;
 import com.jobmatcher.jobmatcher_backend.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findAll();
 }
